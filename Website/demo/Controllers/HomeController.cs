33168309c8de8507f3430f422fef40700840f9b6
Nathanael G <47373466+StrainedQuacking@users.noreply.github.com>
2020-04-19 14:37:10 -0500

    Initial transactions page with dummy data

--- Website/demo/Controllers/HomeController.cs
+++ Website/demo/Controllers/HomeController.cs
@@ -28,6 +28,35 @@ namespace demo.Controllers
             return View();
         }
 
+        public IActionResult Transactions()
+        {
+            var model = new List<Transactions>();
+
+            var transaction1 = new Transactions();
+            transaction1.transactionID = 1234;
+            transaction1.account_number = 1;
+            transaction1.amount = 2000;
+            transaction1.processing_date = "12/12/2020";
+            transaction1.deposit_withdrawal = "Deposit";
+            transaction1.description = "Initial deposit";
+            transaction1.balance = 2000;
+
+            var transaction2 = new Transactions();
+            transaction2.transactionID = 1235;
+            transaction2.account_number = 1;
+            transaction2.amount = -500;
+            transaction2.processing_date = "12/14/2020";
+            transaction2.deposit_withdrawal = "Withdrawal";
+            transaction2.description = "Bill Payment";
+            transaction2.balance = 1500;
+
+            model.Add(transaction1);
+            model.Add(transaction2);
+
+            model.Sort((a, b) => b.processing_date.CompareTo(a.processing_date));
+            return View(model);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Website/demo/Models/Home/Transactions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace demo.Models
+{
+    public class Transactions
+    {
+
+
+        public int account_number { get; set; }
+
+        public int transactionID { get; set; }
+
+        public string processing_date { get; set; }
+
+        public int balance { get; set; }
+
+        public string deposit_withdrawal { get; set; }
+
+        public double amount { get; set; }
+
+        public string description { get; set; }
+
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+
+        }
+    }
+}
